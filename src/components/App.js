176b9233aead72b08ce3afcbe86db0673e81e171
Corey Schadeck <corey@schadeck.com>
2022-05-24 11:59:05 -0400

    commit tree 2f0f41e67f4a11ebc9bfacbe6fa570b1e3458f99

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [puppies, setPuppies] = useState(null);
+        
+    useEffect(() => {
+        fetch("https://images.dog.ceo/breeds/bulldog-english/mami.jpg")
+        .then((response) => response.json())
+        .then((data) => {
+            setPuppies(data.people);
+            console.log(data.people)
+        });
+    }, []);
+
+  if (!setPuppies) return <p>Loading...</p>;
+  return <img src={puppies} alt="A Random Dog" />;
+}
+
+export default App;
+
+
